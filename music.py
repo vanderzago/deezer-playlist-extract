77dda0ceada29330069fad1013ec59999c28d1e1
Vander Alves <vanderzago@gmail.com>
2020-01-01 19:05:26 -0300

    first commit

---
+++ music.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+
+import json
+
+f=open("files/script.json", "r")
+if f.mode == 'r':
+    data =f.read()
+f.close()
+
+f=open("files/playlist.txt", "a+")
+
+response = json.loads(data)
+
+playlist=response['DATA']['TITLE']
+
+track_number=1
+for doc in response['SONGS']['data']:
+    content=playlist+","+str(track_number)+","+doc['ART_NAME']+","+doc['SNG_TITLE']+"\n"
+    print(content)
+    f.write(content)
+    track_number += 1
+f.close()
